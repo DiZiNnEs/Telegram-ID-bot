1c6e0410218b6bff8c7d4a9eb160ea22bbf61bc2
DiZiNnEs <dizinnes228@mail.ru>
2019-11-07 20:23:15 +0600

    release V1

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 from config import bot
-
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot import types
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -10,19 +9,13 @@ def handle_start(message):
     user_markup.row('👤Profile', '⚙️Help')
     user_markup.row('👨‍👨‍👧‍👧Information about chat')
     user_markup.row('🚫Hide menu')
+    user_markup.row('🛠 Bot Information')
     bot.send_message(message.from_user.id, '👋Hello, ' + message.from_user.username, reply_markup=user_markup)
 
 
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     markup = types.ReplyKeyboardMarkup()
-#     profile = types.KeyboardButton(text='👤Information about your profile')
-#     help = types.KeyboardButton(text='Help')
-#     markup.add(profile, help)
-#     msg = bot.send_message(message.chat.id, 'The bot started successfully', reply_markup=markup)
-
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
+    #Handler for profile
     if message.text == '👤Profile':
         #bot.send_message(message.chat.id, 'Your profile ID: ' + str(message.chat.id),)
         bot.send_message(message.chat.id, f'''\
@@ -35,6 +28,8 @@ Surname: {message.from_user.last_name}
 Username : @{message.from_user.username}
 Country code: {message.from_user.language_code}
         ''')
+
+    # Handler for help
     elif message.text == '⚙️Help':
         bot.send_message(message.chat.id, f'''
 ⚙️Help
@@ -45,6 +40,7 @@ Country code: {message.from_user.language_code}
 /me - Profile
 /chat_info - Information about chat
         ''')
+    # Handler for information about chat
     elif message.text == '👨‍👨‍👧‍👧Information about chat':
         bot.send_message(message.chat.id, f'''
 👨‍👨‍👧‍👧Information about chat:
@@ -52,42 +48,57 @@ Country code: {message.from_user.language_code}
 id chat: {message.chat.id}
 Chat type: {message.chat.type}
         ''')
+    # Handler for bot information
+    elif message.text == '🛠 Bot Information':
+        bot.send_message(message.chat.id, f'''
+The bot is designed for people and other developers.
+Please do not scold!
+Developer of this bot: @dizzinnes
+GitHub: https://github.com/DiZiNnEs
+''')
+    # Handler for hide menu
     elif message.text == '🚫Hide menu':
-        pass
-    #else:
-        #bot.send_message(message.chat.id, "Sorry, i don't understand you! Write /help ") # Выводит лишь ошибку
+        bot.send_message(message.chat.id, f'''
+The menu is hidden so that the menu appears again write /start
+        ''')
+    # Handler for /help command
+    elif message.text == '/help':
+        bot.send_message(message.chat.id, f'''
+⚙️ Help
 
-@bot.message_handler(commands=['help'])
-def handler_help(message):
-    bot.send_message(message.chat.id, f'''
-📍 В этом разделе будут представленны все команды, с помощью которых вы сможете управлять ботом.
+📍 In this section, all the commands with which you can control the bot will be presented.
 
-/start - Главное меню
-/help - Помощь
-/me - Профиль
-/chat_info - Информация о чате
-    ''')
-
-@bot.message_handler(commands=['me'])
-def handler_me(message):
-    bot.send_message(message.chat.id, f'''
-   Profile:
+/start - Main menu
+/help - Help
+/me - Profile
+/chat_info - Chat information
+''')
+    # Handler for /me command
+    elif message.text == '/me':
+        bot.send_message(message.chat.id, f'''\
+        👤 Profile:
 
 Your ID: {message.chat.id}
 This is a bot: {message.from_user.is_bot} 
 Name: {message.from_user.first_name}
 Surname: {message.from_user.last_name}
 Username : @{message.from_user.username}
-Country code: {message.from_user.language_code} 
-    ''')
-# @bot.message_handler(commands=['my_id'])
-# def my_id(message):
-#     bot.send_message(message.chat.id, 'Your ID in telegram: ' + str(message.chat.id))
-#
-#
-# @bot.message_handler(content_types=['sticker'])
-# def sticker(message):
-#     bot.send_message(message.chat.id, 'Sticker ID: ' + str(message.sticker.file_id))
+Country code: {message.from_user.language_code}
+                ''')
+    # Handler for /chat_info command
+    elif message.text == '/chat_info':
+        bot.send_message(message.chat.id, f'''
+  👨‍👨‍👧‍👧Information about chat:
+
+id chat: {message.chat.id}
+Chat type: {message.chat.type}
+                ''')
+    else:
+        bot.send_message(message.chat.id, "Sorry, i don't understand you! Write /help ")
+
+@bot.message_handler(content_types=['sticker'])
+def handler_sticker(message):
+    bot.send_message(message.chat.id, 'Sticker ID: ' + message.sticker.file_id)
 
 
 bot.polling(interval=0, none_stop=())
